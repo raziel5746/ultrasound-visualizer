fa0c76ebdad0c05d889dba75dbb607a2985c5025
razielDE <raziel5746@gmail.com>
2024-10-20 20:39:07 -0300

    Moved from ThreeJS To BabylonJS. So much better!

---
+++ src/components/ColorPalette.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ColorPalette = ({ colors, selectedColor, onColorSelect }) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '15px' }}>
+      {colors.map((color, index) => (
+        <div
+          key={index}
+          onClick={() => onColorSelect(color)}
+          style={{
+            width: '30px',
+            height: '30px',
+            borderRadius: '50%',
+            backgroundColor: color,
+            cursor: 'pointer',
+            border: selectedColor === color ? '2px solid #fff' : 'none',
+            boxShadow: selectedColor === color ? '0 0 0 2px #000' : 'none',
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ColorPalette;
